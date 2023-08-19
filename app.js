2f2e04eb07a14f91a83645c621b2744cbfdc74dc
Kamil <kamilczekotas@gmail.com>
2023-08-20 00:58:41 +0200

    test idea done

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
 $(document).ready(function() {
+  $('#result').hide();
     $(window).keydown(function(event){
       if(event.keyCode == 13) {
         event.preventDefault();
         return false;
       }
     });
+    $('form').submit(function(){
+      event.preventDefault();
+      const keywords = [];
+      let fullText = $("#full").val();
+
+      if(!$('li').length) {
+        alert('Insert keywords');
+      }else {
+        $('#result').show();
+        $('li').each(function() {
+          keywords.push($(this).text());
+        })
+        $.each(keywords, function(i, val) {
+          if (fullText.indexOf(val) >= 0){
+            $('#result').append('<li>' + val + ', FOUND</li>');
+          }else{
+            $('#result').append('<li>' + val + ', NOT FOUND</li>');
+          }
+        })
+      }
+    });
 });
 
 function search(e) {
     if(event.key === 'Enter') {
-        $("#keywordList").append('<li>mod_' + (e.value) + '</li>');    
+        $('#keywordList').append('<li>mod_' + (e.value) + '</li>');    
         $('#keyword').val("");
     }
 }
